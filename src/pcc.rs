ef13b0c569613c1f89f370169fdfab3546d8a831
p3zhy <pejman.ghd@gmail.com>
2024-03-24 16:27:09 +0330

    Using vector instead of fixed-size array in pcc

--- src/pcc.rs
+++ src/pcc.rs
@@ -1,7 +1,6 @@
 // The physical speed limit method or Physical Cross-Check for Speed Over Ground (PCC_sog)
 //  validates that the vessel stays in reasonable speed ranges.
-
-use crate::utilities;
+use crate::*;
 use core::time::Duration;
 pub struct PhysicalSpeedLimit {
     max_speed: f32,
@@ -69,12 +68,12 @@ impl PhysicalRateOfTurnLimit {
     }
 }
 
-pub struct PhysicalEnvironmentLimitMethod<const M: usize, const N: usize> {
-    pub polygons: [Polygon<N>; M],
+pub struct PhysicalEnvironmentLimitMethod {
+    pub polygons: Vec<Polygon>,
 }
 
-impl<const M: usize, const N: usize> PhysicalEnvironmentLimitMethod<M, N> {
-    fn new(polygons: [Polygon<N>; M]) -> Self {
+impl PhysicalEnvironmentLimitMethod {
+    fn new(polygons: Vec<Polygon>) -> Self {
         Self { polygons }
     }
     pub fn is_spoofing_attack(&self, point: Point) -> bool {
@@ -102,31 +101,34 @@ impl Point {
     }
 }
 
-#[derive(Debug)]
-pub struct Polygon<const N: usize> {
-    pub points: [Point; N],
-    multipliers: [f32; N],
-    constants: [f32; N],
+pub struct Polygon {
+    pub points: Vec<Point>,
+    multipliers: Vec<f32>,
+    constants: Vec<f32>,
 }
 
-impl<const N: usize> Polygon<N> {
-    fn new(points: [Point; N]) -> Self {
+impl Polygon {
+    fn new(points: Vec<Point>) -> Self {
         let n = points.len();
         let mut j = n - 1;
-        let mut multipliers: [f32; N] = [0.0; N]; // Initialize arrays with default values
-        let mut constants: [f32; N] = [0.0; N];
-        for i in 0..points.len() {
+        let mut multipliers = Vec::with_capacity(n);
+        let mut constants = Vec::with_capacity(n);
+        for i in 0..n {
             if points[j].latitude == points[i].latitude {
-                constants[i] = points[i].longitude;
-                multipliers[i] = 0.0;
+                constants.push(points[i].longitude);
+                multipliers.push(0.0);
             } else {
-                constants[i] = points[i].longitude
-                    - (points[i].latitude * points[j].longitude)
-                        / (points[j].latitude - points[i].latitude)
-                    + (points[i].latitude * points[i].longitude)
-                        / (points[j].latitude - points[i].latitude);
-                multipliers[i] = (points[j].longitude - points[i].longitude)
-                    / (points[j].latitude - points[i].latitude);
+                constants.push(
+                    points[i].longitude
+                        - (points[i].latitude * points[j].longitude)
+                            / (points[j].latitude - points[i].latitude)
+                        + (points[i].latitude * points[i].longitude)
+                            / (points[j].latitude - points[i].latitude),
+                );
+                multipliers.push(
+                    (points[j].longitude - points[i].longitude)
+                        / (points[j].latitude - points[i].latitude),
+                );
             }
             j = i;
         }
@@ -136,13 +138,14 @@ impl<const N: usize> Polygon<N> {
             constants,
         }
     }
+
     pub fn is_inside(&self, point: &Point) -> bool {
         let mut odd_nodes = false;
         let n = self.points.len();
-        let mut current: bool = self.points[n - 1].latitude > point.latitude;
-        let mut prv: bool;
+        let mut current = self.points[n - 1].latitude > point.latitude;
+        let mut prv;
 
-        for i in 0..self.points.len() {
+        for i in 0..n {
             prv = current;
             current = self.points[i].latitude > point.latitude;
             if current != prv {
@@ -150,6 +153,6 @@ impl<const N: usize> Polygon<N> {
                     point.latitude * self.multipliers[i] + self.constants[i] < point.longitude;
             }
         }
-        return odd_nodes;
+        odd_nodes
     }
 }
